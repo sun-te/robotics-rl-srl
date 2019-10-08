bec364e4073e54ca7d48ada77b6acee04905a43a
sun-te <sun_te@outlook.com>
2019-10-09 00:17:12 +0800

    urdf treatement

--- environments/sjtubot_gym/bulletlearn.py
+++ environments/sjtubot_gym/bulletlearn.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 from termcolor import colored
+import os
 from ipdb import set_trace as tt
 def printGreen(string):
     """
@@ -30,17 +31,26 @@ def printBlue(string):
     :param string: (str)
     """
     print(colored(string, 'blue'))
-# physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+
+
+_urdf_path = pybullet_data.getDataPath()
+custom_urdf_path = "/home/tete/work/SJTU/kuka_play/robotics-rl-srl/urdf"
+sjtu_urdf_path = "/home/tete/work/SJTU/kuka_play/robotics-rl-srl/urdf_robot"
+# physicsClient = p.connect(p.DIRECT)#or p.DIRECT for non-graphical version
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
-planeId = p.loadURDF("plane.urdf")
+# p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+
+#p.setAdditionalSearchPath('urdf') #optionally
+p.setGravity(0,0,0)
+
+planeId = p.loadURDF(os.path.join(_urdf_path, "plane.urdf"))
 
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
-racerId = p.loadURDF("racecar/racecar.urdf", [1,1,1], cubeStartOrientation)
-
+boxId = p.loadURDF(os.path.join(_urdf_path, "r2d2.urdf"), cubeStartPos, cubeStartOrientation)
+# modelId = p.loadURDF(os.path.join(_urdf_path, "kuka_iiwa/model_vr_limits.urdf"), [1,1,1])
+# objects = p.loadSDF(os.path.join(_urdf_path, "kuka_iiwa/kuka_with_gripper2.sdf"))
+sjtuID = p.loadURDF(os.path.join(sjtu_urdf_path,"inmoov.urdf"), [1,1,1] )
 for i in range (10000):
     p.stepSimulation()
 time.sleep(1.)
